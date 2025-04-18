8139c5fe75418c3a7edb8541d7720dbeab34f013
Rohit Saini <70269264+rohitsaini81@users.noreply.github.com>
2025-04-18 12:57:49 +0530

    Update Footer.jsx

--- react-portfolio/src/Components/Pages/Footer.jsx
+++ react-portfolio/src/Components/Pages/Footer.jsx
@@ -20,7 +20,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/rohitsaini81"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -30,7 +30,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://twitter.com/rohitsaini81"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -40,7 +40,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/rohitsaini81/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -50,7 +50,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/scientist.tech"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
